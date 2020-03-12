13effc5827aef63d7fb8bb2e155c78854cd19fe7
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-12 14:04:29 -0400

    Almost Finished

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ function renderToys(element) {
   
     let newDiv = document.createElement('div')
 
+    newDiv.dataset.id = element.id
     let toyName = document.createElement('h2')
       toyName.innerText = element.name
       
@@ -36,12 +37,13 @@ function renderToys(element) {
       toyImgSrc.className = 'toy-avatar'
 
     let toyLikeP = document.createElement('p')
-      let likeCount = 0
+      toyLikeP.innerText = `${element.likes}`
       
-      let toyLikeBtn = document.createElement('button')
+    let toyLikeBtn = document.createElement('button')
+      toyLikeBtn.dataset.id = element.id
       toyLikeBtn.className = 'like-btn'
       toyLikeBtn.innerText = "Like <3"
-      toyLikeBtn.addEventListener("click", () =>  toyLikeP.innerText = `${likeCount += 1} Like(s)`)
+      toyLikeBtn.addEventListener("click", increaseLikes)
 
     newDiv.classList += 'card'
     newDiv.append(toyName, toyImgSrc, toyLikeP, toyLikeBtn)
@@ -66,4 +68,26 @@ function processNewToy(event) {
     )}).then(resp => resp.json())
     .then(resp => renderToys(resp))
     event.target.reset()
+}
+
+function increaseLikes(event) {
+  event.preventDefault()
+  let toyLikeId = event.currentTarget.dataset.id
+  // document.getElementById("myDIV").querySelectorAll(".example")
+  let element = event.target.parentElement
+  let newLikes = parseInt(element.children[2].innerText)
+  newLikes += 1
+  fetch(`http://localhost:3000/toys/${toyLikeId}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    
+    body: JSON.stringify({
+      "likes": newLikes
+    })
+  })
+  .then(resp => resp.json())
+  .then(resp => renderToys(resp))
 }
