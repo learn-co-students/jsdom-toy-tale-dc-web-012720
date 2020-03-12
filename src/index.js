2d21940982d1137bc680b532f77c2a1db51105ac
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-12 11:34:21 -0400

    Card Completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +14,34 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(resp => renderToys(resp))
+}
+
+function renderToys(input) {
+  let toyCollectDiv = document.getElementById('toy-collection')
+  input.forEach(element => {
+    let newDiv = document.createElement('div')
+
+    let toyName = document.createElement('h2')
+      toyName.innerText = element.name
+      
+    let toyImgSrc = document.createElement('img')
+      toyImgSrc.src = element.image
+      toyImgSrc.className = 'toy-avatar'
+
+    let toyLikeP = document.createElement('p')
+
+    let toyLikeBtn = document.createElement('button')
+      toyLikeBtn.className = 'like-btn'
+      toyLikeBtn.innerText = "Like <3"
+
+    newDiv.classList += 'card'
+    newDiv.append(toyName, toyImgSrc, toyLikeP, toyLikeBtn)
+    toyCollectDiv.append(newDiv)
+  });
+}
